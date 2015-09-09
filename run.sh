6a78b01418cdbfd498ac45f2d1f3a64a8a8ab6bb
Dominik Hastrich <dominik@MacBook-Pro.fritz.box>
2015-09-09 19:59:34 +0200

    Add setup

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "OK, you're in"
+mkdir -p /tmp
+echo "container running ..." >> /tmp/app.log
+echo `date` >>  /tmp/app.log
+echo "check watchman trigger"
+watchman trigger-list /data/services
+echo "let's read the log"
+tail -f /tmp/app.log
