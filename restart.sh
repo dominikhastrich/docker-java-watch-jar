b0b6b7e57675b76f34d1bd9c8421eeca2ab3d188
Dominik Hastrich <dominik@MacBook-Pro.fritz.box>
2015-09-11 10:28:03 +0200

    Optimize logging

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo -e "\n---\n/restart.sh\n\nlet's get rolling ...\n\nkilling all running processes of jar files\n" >> /tmp/app.log
+echo -e "\n---\n`date`: /restart.sh\n\nlet's get rolling ...\n`date`: killing all running processes of jar files\n" >> /tmp/app.log
 ps -ef | grep "java -jar" | grep "jar" >> /tmp/app.log
 ps -ef | grep "java -jar" | grep "jar" | awk '{print $2}' | xargs kill >> /tmp/app.log 2>&1
-echo -e "\n\nstart up target jar file\n\njava -jar /data/services/$TARGET_JAR $TARGET_ARGS" >> /tmp/app.log 2>&1 &
-java -jar /data/services/$TARGET_JAR $TARGET_ARGS & 
-echo -e "\n\nfinished\n---\n" >> /tmp/app.log
+echo -e "\n\n`date`: start up target jar file\n\njava -jar /data/services/$TARGET_JAR $TARGET_ARGS" >> /tmp/app.log
+java -jar /data/services/$TARGET_JAR $TARGET_ARGS >> /tmp/app.log 2>&1 &
+echo -e "\n\n`date`: finished\n---\n" >> /tmp/app.log
